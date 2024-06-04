fb7369b32e52a4206f92f57489b3d096745945f5
akshitagarg275 <akshitagarg275@gmail.com>
2024-06-04 20:55:45 +0530

    day 5: conditionals complete

--- 02conditionals/01control_statements.py
+++ 02conditionals/01control_statements.py
@@ -13,7 +13,7 @@ if (condition) :
     # if statements
 '''
 
-age = 18
+age = 13
 
 # if age >= 18:
 #     print("You can drive the car!")
@@ -30,3 +30,125 @@ age = 18
 # TODO: WAP to find whether the given no is even or odd
 # TODO: WAP to find if the person can vote or not
 
+# TODO: WAP to find whether a given no is +ve, -ve or 0
+
+# num = 0
+
+# if num > 0:
+#     print("It is a positive number")
+# elif num < 0:
+#     print("It is negative number")
+# elif num == 0:
+#     print("Zero")
+
+# TODO: Rating System
+# rating = int(input("Please enter the rating 1-5: "))
+
+# if rating == 5:
+#     print("Awesome Work!")
+# elif rating == 4:
+#     print("Good Work")
+# elif rating == 3:
+#     print("You can do it")
+# elif rating == 2:
+#     print("Need more hard work")
+# elif rating == 1:
+#     print("Not so good")
+# else:
+#     print("Enter the rating b/w 1-5")
+
+# if rating == 5:
+#     print("Awesome Work!")
+# if rating == 4:
+#     print("Good Work")
+# if rating == 3:
+#     print("You can do it")
+# if rating == 2:
+#     print("Need more hard work")
+# if rating == 1:
+#     print("Not so good")
+# else:
+#     print("Enter the rating b/w 1-5")
+
+
+# TODO: grading System
+
+
+# TODO: nested if-else
+'''
+if (condition):
+    # if (condition):
+        # if statement
+    else:
+        # else statement
+else:
+    # if (condition):
+        # if statement
+'''
+
+# WAP to find greatest among three nos
+
+num1 = 5
+num2 = 6
+num3 = 1
+
+# if num1 >= num2:
+#     if num1 >= num3:
+#         print("num1 is greatest")
+#     else:
+#         print("num3 is greatest")
+# else:
+#     if num2 >= num3:
+#         print("Num2 is greatest")
+#     else:
+#         print("Num 3 is greatest")
+
+# if (num1 >= num2) and (num1 >= num3):
+#     print("num1 is greatest")
+# elif (num2 >= num3) and (num2 >= num1):
+#     print("Num 2 is greatest")
+# else:
+#     print("Num 3 is greatest")
+
+'''
+Google, Facebook, manual
+'''
+
+isGoogle = True
+isFacebook = False
+isEmail = False
+
+# if isGoogle or isEmail or isFacebook:
+#     print("You are signed in")
+# else:
+#     print("Not signed in")
+
+# if isGoogle:
+#     print("Signed in using google")
+# elif isFacebook:
+#     print("Signed in with Facebook")
+# elif isEmail:
+#     print("signed in with email")
+
+
+'''
+Purchasing an item
+signedIn,cart,Payment 
+'''
+
+isSignedIn = True
+isCart = True
+isPaid = True
+
+
+if isSignedIn:
+    if isCart:
+        if isPaid:
+            print("You can make the purchase!")
+        else:
+            print("Make the payment")
+    else:
+        print("Add items into the cart")
+else:
+    print("Please sign in")
+    
