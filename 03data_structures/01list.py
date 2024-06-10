81bcd8ce3a32164a3d47e86a336b316b99c20e33
akshitagarg275 <akshitagarg275@gmail.com>
2024-06-10 20:57:26 +0530

    Day 9: lists

--- 03data_structures/01list.py
+++ 03data_structures/01list.py
@@ -32,7 +32,7 @@
 # print(even)
 
 # TODO: slicing 
-fruits = ["apple", "guava", "banana", "orange", "papaya", "blueberry"]
+# fruits = ["apple", "guava", "banana", "orange", "papaya", "blueberry" , "guava"]
 
 
 # listName[start_index : end_index: step]
@@ -41,6 +41,52 @@ fruits = ["apple", "guava", "banana", "orange", "papaya", "blueberry"]
 # print(fruits[1: 6 : 2])
 # print(fruits[ : 4])
 # print(fruits[: :])
-print(fruits[-5 : -2])
+# print(fruits[-5 : -2])
 
 
+# TODO: append -> adding the element at the end of the list
+# print("before: ", fruits)
+# fruits.append("cherry")
+# print("after: ", fruits)
+
+# fruits.clear()
+# del(fruits)
+# print("after: ", fruits)
+
+# print(fruits.index("guava"))
+
+fruits = ["apple", "guava", "banana", "orange", "papaya", "blueberry" , "guava"]
+# fruits.insert(2,"mango")
+# print(fruits)
+
+# fruits.extend(["strawberry", "water melon", "musk melon"])
+# print(fruits)
+
+# TODO: count : to know the no of objects
+# print(fruits.count("guava"))
+
+# print(fruits.remove("banana"))
+# print(fruits)
+
+# while "guava" in fruits:
+#     fruits.remove("guava")
+
+# print(fruits)
+# print("before: ", fruits)
+# print(fruits.pop())
+# print("after: ", fruits)
+
+# TODO: reverse
+fruits = ["apple", "guava", "banana", "orange", "papaya", "blueberry" , "guava"]
+
+# print(fruits.reverse())
+# print(reversed(fruits))
+# print(fruits)
+
+
+# TODO: sort
+print("before sorting: ", fruits)
+# print(fruits.sort())
+# print(sorted(fruits))
+fruits = sorted(fruits, reverse=True)
+print("after sorting: ", fruits)
