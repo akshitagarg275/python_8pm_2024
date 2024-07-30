3ccfc4d15f92e8fecb3e86790fcd7a54311eb55c
akshitagarg275 <akshitagarg275@gmail.com>
2024-07-30 20:55:59 +0530

    day 30 and 31: models and admin customisation

--- django/mysite/myapp/admin.py
+++ django/mysite/myapp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from .models import ChaiVariety, ChaiReview, Store, ChaiCertificate
 # Register your models here.
+
+class ChaiReviewInline(admin.TabularInline):
+    model = ChaiReview
+    extra = 2
+
+class ChaiVarietyAdmin(admin.ModelAdmin):
+    list_display = ('tea_name', 'type', 'date_added')
+    inlines = [ChaiReviewInline]
+
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ('chai', 'certificate_number')
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_vertical = ('chai_varieties',)
+
+
+admin.site.register(ChaiVariety, ChaiVarietyAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(ChaiCertificate, ChaiCertificateAdmin)
