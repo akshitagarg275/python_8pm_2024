01c37c2bfe133d5b7593b3ea57b36d5dbbacf942
akshitagarg275 <akshitagarg275@gmail.com>
2024-08-01 20:59:35 +0530

    day 32: queries and forms

---
+++ django/mysite/chai/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, get_object_or_404
+from myapp.models import ChaiVariety, Store
+from .forms import ChaiVarietyForm
+# Create your views here.
+
+def all_chai(request):
+    chais = ChaiVariety.objects.all()
+    return render(request, 'chai/all_chai.html', {'chais': chais})
+    # return render(request, 'dummy.html', {'chais': chais})
+
+def chai_detail(request, chai_id):
+    chai = get_object_or_404(ChaiVariety, pk = chai_id)
+    return render(request, 'chai/chai_detail.html', {'chai': chai})
+
+
+def all_chai_stores(request):
+    stores = None
+    if request.method == 'POST':
+        form = ChaiVarietyForm(request.POST)
+        if form.is_valid():
+            chai_variety = form.cleaned_data['chai_variety']
+            stores = Store.objects.filter(chai_varieties = chai_variety)
+    else:
+        form = ChaiVarietyForm()
+    return render(request, 'chai/chai_stores.html', {'stores': stores, 'form':form})
